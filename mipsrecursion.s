aa9396f6d8bd46d3e65608c49a878a6130a36ffd
Ternia Wilson <terniawilson@Ternias-MBP.hsd1.wa.comcast.net>
2020-03-31 19:10:15 -0700

    data section w array

--- mipsrecursion.s
+++ mipsrecursion.s
@@ -1,8 +1,9 @@
-{\rtf1\ansi\ansicpg1252\cocoartf1671\cocoasubrtf600
-{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww10800\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+# Ternia Wilson
+# 02894922 % 11 = 8
+# 8 + 26 = Base 34
 
-\f0\fs24 \cf0 Mips recursion}
+.data  # data section
+    myMessage: .asciiz "Please enter a string of up to 100 characters: \n"
+    myArray: .space 101 # this creates space for the users input
+    invalidMessage: .asciiz "Invalid input\n"
+    array: .word 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 , 0 # space for an array of 20 elements
